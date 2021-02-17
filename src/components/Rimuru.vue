0c48bdbf0b362d02557c54b3e0a2db2ae6fca020
lunaisnotaboy <her@mint.lgbt>
2021-02-17 10:14:59 -0500

    Various things, added Tailwind CSS, added netlify.toml

---
+++ src/components/Rimuru.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="grid gap-4 grid-cols-3">
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div><img src="../assets/rimuru.png" alt="rimuru" width="25%" /></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+</template>
+
+<script setup>
+// import { defineProps, reactive } from 'vue'
+
+// defineProps({
+//   msg: String
+// })
+
+// const state = reactive({ count: 0 })
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
