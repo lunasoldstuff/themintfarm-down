24ab1b155b17a1159a5f441eb1e28877236a9d6c
lunaisnotaboy <her@mint.lgbt>
2021-02-17 10:28:08 -0500

    Added new page

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,10 @@ module.exports = {
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      padding: ['hover'],
+      translate: ['motion-reduce'],
+    },
   },
   plugins: [],
 }
