856ea9067ca493c79230f5f65147c6f925e19529
lunaisnotaboy <her@mint.lgbt>
2021-02-17 10:33:20 -0500

    add dark mode config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     enabled: true,
     content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   },
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
   },
